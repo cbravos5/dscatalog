eff48bf6cc89853a499c870dd274f3f70525f880
cbravos <cnbravos5@gmail.com>
2023-05-07 13:20:02 -0300

    feat: add JPA and H2 dependencies; add appilcation properties

--- src/main/java/com/dscatalog/entities/Category.java
+++ src/main/java/com/dscatalog/entities/Category.java
@@ -3,7 +3,17 @@ package com.dscatalog.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
