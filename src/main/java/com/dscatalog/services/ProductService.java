4f8aad69f0ce3c20e86eee08cc8152e8757810c7
cbravos <cnbravos5@gmail.com>
2023-06-28 19:01:04 -0300

    feat: add Product insert

--- src/main/java/com/dscatalog/services/ProductService.java
+++ src/main/java/com/dscatalog/services/ProductService.java
@@ -12,8 +12,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dscatalog.dto.ProductDTO;
+import com.dscatalog.dto.CategoryDTO;
 import com.dscatalog.dto.NewProductDTO;
+import com.dscatalog.entities.Category;
 import com.dscatalog.entities.Product;
+import com.dscatalog.repositories.CategoryRepository;
 import com.dscatalog.repositories.ProductRepository;
 import com.dscatalog.services.exceptions.DatabaseException;
 import com.dscatalog.services.exceptions.ResourceNotFoundException;
@@ -24,6 +27,9 @@ import jakarta.persistence.EntityNotFoundException;
 public class ProductService {
   @Autowired
   private ProductRepository repository;
+
+  @Autowired
+  private CategoryRepository categoryRepository;
 
   @Transactional(readOnly = true)
   public List<ProductDTO> findAll() {
@@ -45,6 +51,8 @@ public class ProductService {
   public ProductDTO insert(NewProductDTO request) {
     var product = new Product();
 
+    copyDTOToEntity(request, product);
+
     var newProduct = repository.save(product);
 
     return new ProductDTO(newProduct);
@@ -54,10 +62,12 @@ public class ProductService {
   public ProductDTO update(Long id, NewProductDTO request) {
     try {
       Product product = repository.getReferenceById(id);
-  
+
+      copyDTOToEntity(request, product);
+
       product.setName(request.name());
       Product updatedCaregory = repository.save(product);
-  
+
       return new ProductDTO(updatedCaregory);
     } catch (EntityNotFoundException e) {
       throw new ResourceNotFoundException("Id not found " + id);
@@ -69,7 +79,7 @@ public class ProductService {
       repository.deleteById(id);
     } catch (EmptyResultDataAccessException e) {
       throw new ResourceNotFoundException("Id not found " + id);
-    } catch(DataIntegrityViolationException e) {
+    } catch (DataIntegrityViolationException e) {
       throw new DatabaseException("Integrity violation");
     }
   }
@@ -79,5 +89,19 @@ public class ProductService {
     Page<Product> categories = repository.findAll(pageRequest);
 
     return categories.map(ProductDTO::new);
+  }
+
+  private void copyDTOToEntity(NewProductDTO productDTO, Product entity) {
+    entity.setDescription(productDTO.description());
+    entity.setImgUrl(productDTO.imgUrl());
+    entity.setName(productDTO.name());
+    entity.setPrice(productDTO.price());
+
+    entity.getCategories().clear();
+
+    for (CategoryDTO c : productDTO.categories()) {
+      Category category = categoryRepository.getReferenceById(c.id());
+      entity.getCategories().add(category);
+    }
   }
 }
