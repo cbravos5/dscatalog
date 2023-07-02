df559617b9273a0478e108868c2b6617acf30c22
cbravos <cnbravos5@gmail.com>
2023-07-02 13:20:36 -0300

    refactor: paged request

--- src/main/java/com/dscatalog/resources/CategoryResource.java
+++ src/main/java/com/dscatalog/resources/CategoryResource.java
@@ -6,8 +6,7 @@ import com.dscatalog.services.CategoryService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -24,21 +22,14 @@ import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/categories")
-public class CategoryResource { 
+public class CategoryResource {
 
     @Autowired
     private CategoryService service;
 
     @GetMapping
-    public ResponseEntity<Page<CategoryDTO>> findAll(
-        @RequestParam(value = "page", defaultValue = "0") Integer page,
-        @RequestParam(value = "pageSize", defaultValue = "12") Integer pageSize,
-        @RequestParam(value = "direction", defaultValue = "ASC") String direction,
-        @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
-    ) {
-        PageRequest pageRequest = PageRequest.of(page, pageSize, Direction.valueOf(direction), orderBy);
-
-        Page<CategoryDTO> list = service.findAllPaged(pageRequest);
+    public ResponseEntity<Page<CategoryDTO>> findAll(Pageable pageable) {
+        Page<CategoryDTO> list = service.findAllPaged(pageable);
 
         return ResponseEntity.ok().body(list);
     }
@@ -55,10 +46,10 @@ public class CategoryResource {
         CategoryDTO category = service.insert(request);
 
         URI uri = ServletUriComponentsBuilder
-                    .fromCurrentRequest()
-                    .path("/{id}")
-                    .buildAndExpand(category.id())
-                    .toUri();
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(category.id())
+                .toUri();
 
         return ResponseEntity.created(uri).body(category);
     }
--- src/main/java/com/dscatalog/services/CategoryService.java
+++ src/main/java/com/dscatalog/services/CategoryService.java
@@ -8,6 +8,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -56,10 +57,10 @@ public class CategoryService {
   public CategoryDTO update(Long id, NewCategoryDTO request) {
     try {
       Category category = repository.getReferenceById(id);
-  
+
       category.setName(request.name());
       Category updatedCaregory = repository.save(category);
-  
+
       return new CategoryDTO(updatedCaregory);
     } catch (EntityNotFoundException e) {
       throw new ResourceNotFoundException("Id not found " + id);
@@ -71,13 +72,13 @@ public class CategoryService {
       repository.deleteById(id);
     } catch (EmptyResultDataAccessException e) {
       throw new ResourceNotFoundException("Id not found " + id);
-    } catch(DataIntegrityViolationException e) {
+    } catch (DataIntegrityViolationException e) {
       throw new DatabaseException("Integrity violation");
     }
   }
 
   @Transactional(readOnly = true)
-  public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+  public Page<CategoryDTO> findAllPaged(Pageable pageRequest) {
     Page<Category> categories = repository.findAll(pageRequest);
 
     return categories.map(CategoryDTO::new);
